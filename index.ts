63f755dd1df86613dea93179623e3cca58f5714d
Maki <maloca1992@gmail.com>
2017-01-30 14:30:34 +0100

    bug(JasperoModule): add FormModule :bug:

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SelectComponent} from './src/select.component';
+import {FormsModule} from '@angular/forms';
 
 export * from './src/select.component';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, FormsModule],
     declarations: [SelectComponent],
     exports: [SelectComponent]
 })
