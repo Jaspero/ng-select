31b4c309226fae5e4f59c2cd7fb447a9f32bdf72
flauc <filip.lauc93@gmail.com>
2018-04-07 14:58:13 +0200

    feat: Working on total refactor :hammer:

--- src/components/select/select.component.ts
+++ src/components/select/select.component.ts
@@ -1,181 +1,266 @@
-import {Component, Input, forwardRef, HostListener, ViewChild, ElementRef} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy, ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  forwardRef,
+  Inject,
+  Input, OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  Renderer2, TemplateRef,
+  ViewChild
+} from '@angular/core';
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+import {fromEvent} from 'rxjs/observable/fromEvent';
+import {filter, skip, takeUntil, withLatestFrom} from 'rxjs/operators';
+import {Subject} from 'rxjs/Subject';
+import {KeyCode} from '../../enums/key-code.enum';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
 @Component({
   selector: 'jaspero-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [{
     provide: NG_VALUE_ACCESSOR,
     useExisting: forwardRef(() => SelectComponent),
     multi: true
   }]
 })
-export class SelectComponent implements ControlValueAccessor {
-  constructor() { }
+export class SelectComponent implements ControlValueAccessor, OnInit, AfterViewInit, OnChanges, OnDestroy {
+  constructor(
+    private _renderer: Renderer2,
+    private _cdr: ChangeDetectorRef,
+    @Inject(DOCUMENT) private _document: any
+  ) { }
 
-  @Input() selected: any;
+  @ViewChild('input') inputEl: ElementRef;
 
-  @Input() set options(sel: any[]) {
-    if (sel) {
-      this.selection = sel;
-      this.formatedSelection = sel.map(item => item);
-      this.filteredSelection = sel.map(item => item);
-    }
-  }
-
-  @Input() matchFromStart = false;
   @Input() key = 'name';
-  @Input() set multi(to: boolean) {
-    this.isMulti = to;
-    if (to) {
-      this.selected = this.selected || [];
-    }
+  @Input() options: any[];
+  @Input() multiSelect: boolean;
+  @Input() theTabIndex: number;
+  @Input() matchFromStart = false;
+  @Input() returnOnlyKey = false;
+  @Input() selected: any;
+  @Input() selectTemplate: TemplateRef<any>;
+  @Input() dropDownTemplate: TemplateRef<any>;
+
+  @Output() toggled = new EventEmitter<boolean>();
+
+  propagateChange;
+  onTouched;
+
+  open$ = new Subject<boolean>();
+  destroyed$ = new Subject();
+
+  isMulti$ = new BehaviorSubject(false);
+  activeIndex$ = new BehaviorSubject(null);
+  selection$ = new BehaviorSubject(null);
+
+  selectToUse: Function = this.select;
+  outputToUse: Function = this.output;
+  removeToUse: Function = this.remove;
+  mapMethodToUse: Function = this.output;
+
+  blockEmit = false;
+
+  ngOnInit() {
+    this.open$.pipe(
+      takeUntil(this.destroyed$)
+    )
+      .subscribe(value => {
+        this.toggled.emit(value);
+      });
+
+    this.selection$.pipe(
+      takeUntil(this.destroyed$),
+      skip(1),
+      filter(() => !this.blockEmit)
+    )
+      .subscribe(value => {
+        this.propagateChange(this.outputToUse(value));
+      });
   }
 
-  @ViewChild('inp') inpEl: ElementRef;
+  ngAfterViewInit() {
+    fromEvent(this._document.documentElement, 'click')
+      .pipe(
+        takeUntil(this.destroyed$)
+      )
+      .subscribe(() => {
+        this.open$.next(false);
+      });
 
-  search = '';
-  activeIndex: any = null;
-  isMulti = false;
-  active = false;
+    fromEvent(this.inputEl.nativeElement, 'blur')
+      .pipe(
+        takeUntil(this.destroyed$)
+      )
+      .subscribe(() => {
+        this.onTouched();
+      });
 
-  selection: any[] = [];
-  formatedSelection: any[] = [];
-  filteredSelection: any[] = [];
+    fromEvent<KeyboardEvent>(this.inputEl.nativeElement, 'keyup')
+      .pipe(
+        takeUntil(this.destroyed$),
+        withLatestFrom(this.open$, this.activeIndex$)
+      )
+      .subscribe(data => {
+        data[0].stopPropagation();
 
-  @HostListener('document:click') close() {
-    this.active = false;
+        switch (data[0].keyCode) {
+          case KeyCode.UpArrow:
+            if (!data[1]) {
+              this.activeIndex$.next(this.options.length - 1);
+              this.open$.next(true);
+            } else {
+              this.activeIndex$.next(data[2] > 0 ? data[2] - 1 : this.options.length - 1);
+            }
+
+            break;
+
+          case KeyCode.DownArrow:
+            if (!data[1]) {
+              this.activeIndex$.next(0);
+              this.open$.next(true);
+            } else {
+              this.activeIndex$.next(data[2] < this.options.length - 1 ? data[2] + 1 : 0);
+            }
+
+            break;
+
+          case KeyCode.Enter:
+            if (data[2] === null || !this.options.length) {
+              return;
+            }
+
+            this.open$.next(false);
+            this.selectToUse(data[2]);
+            this.activeIndex$.next(null);
+
+            break;
+
+          case KeyCode.Escape:
+            if (data[1]) {
+              this.activeIndex$.next(null);
+              this.open$.next(false);
+            }
+            break;
+        }
+      });
   }
 
-  select(index: any, event?: any) {
-    if (event) {
-      event.stopPropagation();
-    }
+  ngOnChanges(change) {
 
-    if (this.isMulti) {
-      this.selected = [...this.selected, this.filteredSelection[index]];
-    } else {
-      if (this.selected) {
-        this.formatedSelection.push(this.selected);
-        this.filteredSelection.push(this.selected);
+    if (change.multiSelect) {
+      this.isMulti$.next(change.multiSelect.currentValue);
+
+      if (change.multiSelect.currentValue) {
+        this.outputToUse = this.returnOnlyKey ? this.outputOnlyKeyMulti : this.output;
+        this.removeToUse = this.removeMulti;
+        this.selectToUse = this.selectMulti;
+      } else {
+        this.outputToUse = this.returnOnlyKey ? this.outputOnlyKey : this.output;
+        this.removeToUse = this.remove;
+        this.selectToUse = this.select;
       }
-      this.selected = this.filteredSelection[index];
-      this.active = false;
     }
 
-    // TODO: Find a better option this is just a temp fix
-    this.formatedSelection.splice(this.formatedSelection.findIndex(item => this.filteredSelection[index][this.key] === item[this.key]), 1);
-    this.filteredSelection.splice(index, 1);
+    if (change.returnOnlyKey) {
+      const multiCurrent = this.isMulti$.getValue();
 
-    this.search = '';
-    this.propagateChange(this.selected);
-  }
+      if (change.returnOnlyKey.currentValue) {
 
-  remove(index: number, event?: any) {
-    if (event) {
-      event.stopPropagation();
-    }
-
-    if (this.isMulti) {
-      this.formatedSelection.push(this.selected[index]);
-      this.filteredSelection.push(this.selected[index]);
-      this.selected.splice(index, 1);
-    } else {
-      this.selected = null;
-    }
-
-    this.propagateChange(this.selected);
-  }
-
-  openActive(event: any) {
-    event.stopPropagation();
-    this.inpEl.nativeElement.focus();
-    this.active = !this.active;
-    this.activeIndex = this.active ? 0 : null;
-  }
-
-  keyUpHandler(event: any) {
-    event.stopPropagation();
-
-    switch (event.keyCode) {
-      case 38:
-        if (!this.active) {
-          this.activeIndex = this.filteredSelection.length - 1;
-          this.active = true;
+        if (multiCurrent) {
+          this.outputToUse = this.outputOnlyKeyMulti;
+          this.mapMethodToUse = this.mapOnlyKeyMulti;
         } else {
-          this.activeIndex = this.activeIndex > 0 ? this.activeIndex - 1 : this.filteredSelection.length - 1;
+          this.outputToUse = this.outputOnlyKey;
+          this.mapMethodToUse = this.mapOnlyKey;
         }
 
-        return;
-
-      case 40:
-        if (!this.active) {
-          this.activeIndex = 0;
-          this.active = true;
-        } else {
-          this.activeIndex = this.activeIndex < this.filteredSelection.length - 1 ? this.activeIndex + 1 : 0;
-        }
-
-        return;
-
-      case 13:
-        if (this.activeIndex === null || !this.filteredSelection.length) {
-          return;
-        }
-
-        this.active = false;
-        this.select(this.activeIndex);
-        this.activeIndex = null;
-        return;
-    }
-  }
-
-  keyDownHandler(event: any) {
-    if (event.keyCode === 8 && this.isMulti && this.search === '' && this.selected.length) {
-      this.remove(this.selected.length - 1);
-    }
-  }
-
-  filterHandler() {
-    if (this.search === '') {
-      this.filteredSelection = this.formatedSelection.map(a => a);
-    } else {
-      if (!this.active) {
-        this.activeIndex = 0;
-        this.active = true;
+      } else {
+        this.outputToUse = this.output;
+        this.mapMethodToUse = this.output;
       }
-
-      this.filteredSelection = this.formatedSelection
-        .filter(value => this.matchFromStart ?
-          value[this.key].indexOf(this.search) === 0 :
-          value[this.key].indexOf(this.search) !== -1
-        );
     }
+
+    if (change.options) {
+
+    }
+
+    console.log('the change', change);
   }
+
+  ngOnDestroy() {
+    this.destroyed$.next();
+  }
+
+  setObservable(name: string, value: any) {
+    this[name].next(value);
+  }
+
+  open(event: KeyboardEvent) {
+    event.stopPropagation();
+    this.inputEl.nativeElement.focus();
+    this.open$.next(true);
+  }
+
+  output(value: any) {
+    return value;
+  }
+
+  outputOnlyKey(value: any) {
+    return value[this.key];
+  }
+
+  outputOnlyKeyMulti(value: any[]) {
+    return value.map(val => val[this.key]);
+  }
+
+  mapOnlyKey(value: any) {
+    return this.options.find(option => option[this.key] === value);
+  }
+
+  mapOnlyKeyMulti(value: any) {
+    return value.map(val => {
+      return this.options.find(option => option[this.key] === val)
+    });
+  }
+
+  select(index: number) {
+    this.selection$.next(this.options[index]);
+  }
+
+  selectMulti(value: any) {}
+
+  remove() {
+    this.selection$.next(null);
+  }
+
+  removeMulti(value: any) {}
 
   writeValue(value: any) {
-    if (value !== undefined) {
-      this.selected = value;
-
-      if (this.isMulti) {
-        this.selected.forEach((item: any) => {
-          const index = this.formatedSelection.findIndex(i => i[this.key] === item[this.key]);
-
-          if (index !== -1) {
-            this.formatedSelection.splice(index, 1);
-            this.filteredSelection.splice(index, 1);
-          }
-        });
-      }
-    }
+    this.blockEmit = true;
+    this.selection$.next(this.mapMethodToUse(value));
+    this.blockEmit = false;
   }
 
-  propagateChange = (_: any) => {};
-
-  registerOnChange(fn: any) {
+  registerOnChange(fn) {
     this.propagateChange = fn;
   }
 
-  registerOnTouched() {}
+  setDisabledState(isDisabled) {
+    this._renderer.setProperty(this.inputEl.nativeElement, 'disabled', isDisabled);
+    this.open$.next(false);
+  }
+
+  registerOnTouched(fn) {
+    this.onTouched = fn;
+  }
 }
